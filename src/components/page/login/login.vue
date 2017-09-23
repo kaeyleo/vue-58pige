ed3b3e7c0cca04b5fcf3081123d677be2dd51a44
steve liao <mynamelky@gmail.com>
2017-09-23 21:26:22 +0800

    add register page

--- src/components/page/login/login.vue
+++ src/components/page/login/login.vue
@@ -4,22 +4,24 @@
       <div class="back">
         <i class="iconfont icon-fanhui"></i>
       </div>
-      <div class="link">注册</div>
+      <div class="link">
+        <router-link to="/reg">注册</router-link>
+      </div>
     </header>
     <div class="logo">
       <img src="../../../assets/logo-text.png" alt="">
     </div>
-    <div class="form">
+    <div class="login-form">
       <div class="input-item">
-        <input type="text" size="11" placeholder="账户手机号" autocomplete="off">
+        <input type="tel" maxlength="11" placeholder="账户手机号" autocomplete="off">
         <i class="iconfont icon-guanbi icon"></i>
       </div>
       <div class="input-item">
         <input type="password" maxlength="16" placeholder="请输入密码" autocomplete="off">
         <i class="iconfont icon-ai-eye icon"></i>
       </div>
-      <a class="button" href="">登录</a>
-      <a class="forget" href="">忘记密码</a>
+      <a class="login-button" href="">登录</a>
+      <a class="forget-pwd" href="">忘记密码</a>
     </div>
   </div>
 </template>
@@ -29,36 +31,6 @@
 </script>
 
 <style lang="scss" scoped>
-@import '../../../style/helper';
-
-.page {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-}
-
-.app-header {
-  padding: 12px $wrap;
-  position: relative;
-  overflow: auto;
-  color: #555;
-  div {
-    display: block;
-  }
-  .back {
-    float: left;
-    .iconfont {
-      font-size: 5vw;
-      line-height: 5vw;
-    }
-  }
-  .link {
-    float: right;
-    font-size: 4vw;
-  }
-}
-
 .logo {
   margin: 6vw 0;
   img {
@@ -68,53 +40,7 @@
   }
 }
 
-.form {
-  margin: 0 auto;
-  width: 90%;
-}
-
-.input-item {
-  position: relative;
-  padding: 3.6vw 0;
-  border-bottom: 1px solid #DFE3EC;
-  input, div {
-    display: inline-block;
-  }
-  input {
-    width: 80%;
-    margin: 0;
-    padding: 0;
-    background-color: transparent;
-    background-color: transparent;
-    font-size: 3.8vw;
-    &:-webkit-autofill {  
-      -webkit-box-shadow: 0 0 0px 1000px #fff inset !important;  
-    }
-  }
-  .icon {
-    position: absolute;
-    top: 4.2vw;
-    right: 0;
-    width: 18px;
-    height: 18px;
-    color: #C4C7CE;
-  }
-}
-
-.button {
-  margin-top: 7vw;
-  display: block;
-  width: 100%;
-  line-height: 11.6vw;
-  text-align: center;
-  font-size: 4vw;
-  font-weight: 600;
-  color: #fff;
-  background-color: #FF6A41;
-  border-radius: 3px;
-}
-
-.forget {
+.forget-pwd {
   margin-top: 3vw;
   float: right;
   font-size: 3.5vw;
---
+++ src/components/page/login/register.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="page">
+    <header class="app-header">
+      <div class="back">
+        <i class="iconfont icon-fanhui"></i>
+      </div>
+      <div class="page-title">注册</div>
+      <div class="link">
+        <router-link to="/login">登录</router-link>
+      </div>
+    </header>
+
+    <div class="step-slide">
+      <div class="login-form step-one">
+        <div class="input-item">
+          <input type="tel" maxlength="11" placeholder="手机号" autocomplete="off">
+          <i class="iconfont icon-guanbi icon"></i>
+        </div>
+        <div class="input-item">
+          <input type="tel" class="code-input" maxlength="8" placeholder="验证码" autocomplete="off">
+          <i class="iconfont icon-guanbi icon icon-clear-phone"></i>
+          <div class="sendMsg-button">获取验证码</div>
+        </div>
+        <a class="login-button" href="">下一步</a>
+      </div>
+      <div class="login-form step-two">
+        <div class="input-item">
+          <input type="password" maxlength="12" placeholder="设置6~12位密码" autocomplete="off">
+          <i class="iconfont icon-guanbi icon"></i>
+        </div>
+        <a class="login-button button-avl" href="">注册</a>
+      </div>
+    </div>  
+
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped>
+.login-form {
+  margin: 0;
+  width: 100%;
+  float: left;
+}
+
+.sendMsg-button {
+  float: right;
+  width: 30%;
+  font-size: 3.6vw;
+  color: #9FA4AE;
+  text-align: center;
+}
+
+.step-one {
+  .code-input {
+    width: 60%;
+  }
+  .icon-clear-phone {
+    right: 32%;
+  }
+}
+
+.step-slide {
+  position: relative;
+  width: 90%;
+  margin: 4vw auto 0 auto;
+}
+
+.step-one, .step-two {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: 0.5s ease-out;
+}
+
+.step-one {
+  transform: translateX(0%);
+}
+
+.step-two {
+  transform: translateX(110%);
+}
+
+.next {
+  .step-one {
+    transform: translateX(-110%);
+  }
+  .step-two {
+    transform: translateX(0%);
+  }
+}
+</style>
